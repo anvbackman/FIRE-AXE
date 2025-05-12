632358fdc5795984a1be2e3434ce5ed6f84e7c22
Maro2207 <Maro2207@student.miun.se>
2025-05-12 16:17:10 +0200

    Added Llamacall

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         GeminiCall geminiCall = new GeminiCall();
+        LlamaCall llamaCall = new LlamaCall();
         String filePath = "Code.txt";
         try {
             geminiCall.send(filePath, "Application12.java"); // Pass the file path and class name
+            llamaCall.send(filePath, "Application12.java"); // Pass the file path and class name
         } catch (IOException e) {
             e.printStackTrace();
         }
